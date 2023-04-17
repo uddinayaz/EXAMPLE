ee13911f2a92e04ad6d40b1d49c01847a51caf5c
uddinayaz <ayazu7034@gmail.com>
2023-04-17 13:05:05 +0530

    EXAMPLE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Data from './data.json'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {
+      Data && Data.map( data => {
+        return(
+         <div key={data.id}>
+         <p><b>{ data.question }</b></p>
+         <p>{ data.answer }</p>
+         {data.bullets}
+         </div>
+        )
+      })
+    }
     </div>
-  );
+  )
 }
 
-export default App;
