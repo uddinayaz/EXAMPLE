aaa2f2e7b03c25b88c7856638c7d25095fe219c3
uddinayaz <ayazu7034@gmail.com>
2023-04-20 21:39:22 +0530

    clickable

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+import { useState } from 'react'
 import React from 'react'
 import Data from './data.json'
+import './App.css'
 
 
 export default function App() {
+   const[selected, setSelected] = useState(null)
+
+   const toggle = (d) => {
+     if(selected === d) {
+       return setSelected(null)
+     }
+     setSelected(d)
+   }
+
   return (
-    <div>
-    {
-      Data && Data.map( data => {
-        return(
-         <div key={data.id}>
-         <p><b>{ data.question }</b></p>
-         <p>{ data.answer }</p>
-         {data.bullets}
-         </div>
-        )
-      })
-    }
+    <div className="wrapper">
+      <div className="accordion">
+
+          {Data.map((data, d) => (
+             <div className="data"> 
+                 <div className="title" onClick={() => toggle(d)}>
+                    <h2>{data.question}</h2>
+                    <span>{selected === d ? '-' : '+'}</span>
+                 </div>
+                 <div className={selected === d ? 'content show' : 'content'}>{data.answer}</div>
+
+              </div> 
+          ))}
+    
+      </div> 
     </div>
   )
 }
